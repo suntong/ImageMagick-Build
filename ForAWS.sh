79816beab7082876127dde1eaaf06dd9b020c37f
suntong <suntong@cpan.org>
2023-10-18 09:33:08 -0400

    - [!] fix ForAWS.sh, aws-layers does not include /opt

--- ForAWS.sh
+++ ForAWS.sh
@@ -1,7 +1,7 @@
 set -ex
 
 sed '/use-checkinstall --build-only/s||build-dir /opt|; /less wget/s//& zip/; /checkinstall-only-beg/,/checkinstall-only-end/d; /install-pip/,$d' Dockerfile > Dockerfile.aws
-printf 'RUN zip -r /root/imei-aws-layers.zip /opt\nRUN zip -vT /root/imei-aws-layers.zip' >> Dockerfile.aws
+printf 'RUN cd /opt && zip -r /root/imei-aws-layers.zip .\nRUN zip -vT /root/imei-aws-layers.zip' >> Dockerfile.aws
 diff -wU 1 Dockerfile Dockerfile.aws || true
 time docker build -t imaws --progress=plain -f Dockerfile.aws . 2>&1 | tee /tmp/build4aws.log; date
 #  --no-cache
